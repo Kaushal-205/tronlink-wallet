d0b3cb1ec935f0d577b4ffff1b249f5a778c9e3c
Kaushal-205 <20bce123@nirmauni.ac.in>
2024-05-01 15:29:54 +0530

    solve error

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,34 @@
 "use client"
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { WagmiConfig, WagmiProvider, useAccount } from 'wagmi'
-import { config } from './config'
-import { ConnectWallet } from './connectWallet'
+import { useWallet } from '@tronweb3/tronwallet-adapter-react-hooks';
+
 
 const queryClient = new QueryClient()
 
 
-function App() {
+export default function App() {
+  const { address, connected, wallet } = useWallet();
   return (
-    <WagmiProvider config={WagmiConfig}>
-      <QueryClientProvider client={queryClient}> 
-        <ConnectWallet /> 
-      </QueryClientProvider> 
-    </WagmiProvider>
+    <main>
+        <div>
+            <h2>Wallet Connection Info</h2>
+            <p>
+                <span>Connection Status:</span> {connected ? 'Connected' : 'Disconnected'}
+            </p>
+            <p>
+                <span>Your selected Wallet:</span> {wallet?.adapter.name}
+            </p>
+            <p>
+                <span>Your Address:</span> {address}
+            </p>
+        </div>
+      
+        <QueryClientProvider client={queryClient}> 
+         
+        </QueryClientProvider> 
+
+
+    </main>
+    
   )
 }
